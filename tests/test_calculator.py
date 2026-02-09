3b3c87fd621830deb4dada3e4e11f4a7e210ce90
MaryamZulfiqar307 <maryamzulfiqar514@gmail.com>
2026-02-10 01:33:23 +0500

    task 2 division in float

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -14,9 +14,13 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(multiply(10, 5), 50)
 
     def test_divide(self):
-        self.assertEqual(divide(10, 2), 5)
+        result = divide(10, 2)
+        self.assertEqual(result, 5.0)
+        self.assertIsInstance(result, float)
+
         with self.assertRaises(ValueError):
             divide(10, 0)
+
     
     def test_power(self):
         self.assertEqual(power(2, 3), 8)
